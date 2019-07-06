f5e7517365649d89d4603704e29f5071808bae7b
Enrique Montemayor <eemontemayor@live.com>
2019-07-06 18:51:51 -0500

    set up db

--- migrations/001.undo.create_capstone1_meals.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE capstone1_meals
-  DROP COLUMN IF EXISTS user_id;
-
-DROP TABLE IF EXISTS capstone1_users;
-
-  DROP TABLE IF EXISTS capstone1_meals;
--- migrations/002.do.create_meals.sql
+++ migrations/002.do.create_meals.sql
@@ -1,17 +1,15 @@
 
 
-CREATE TABLE meal (
-  id SERIAL PRIMARY KEY,
-  meal_name TEXT NOT NULL,
-  on_day DATE, 
-  ingredients TEXT,
+CREATE TABLE "meal" (
+  "id" SERIAL PRIMARY KEY,
+  "meal_name" TEXT NOT NULL,
+  "on_day" DATE, 
+  "ingredients" TEXT,
   image TEXT,
-  date_created TIMESTAMP DEFAULT now() NOT NULL,
-  bookmarked BOOLEAN
+  "date_created" TIMESTAMP DEFAULT now() NOT NULL,
+  "bookmarked" BOOLEAN,
+  "user_id" INTEGER REFERENCES "user"(id)
+    ON DELETE CASCADE NOT NULL
 );
 
 
-ALTER TABLE meal
-  ADD COLUMN
-    user_id INTEGER REFERENCES user(id),
-    ON DELETE SET NULL;
