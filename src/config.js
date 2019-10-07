73ae8e119f7259d832f54916d2f41a589cba3b74
Enrique Montemayor <eemontemayor@live.com>
2019-10-07 17:38:56 -0500

    x

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
-    // TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
     DB_URL: process.env.DB_URL || 'meal_deal@localhost/meal_deal',
-  DB_URL: process.env.DATABASE_URL || 'meal_deal@localhost/meal_deal',
+  // DB_URL: process.env.DATABASE_URL || 'meal_deal@localhost/meal_deal',
 
   }
