2db600a76f00b3081bdd602a61eff0fc57874bd6
Enrique Montemayor <eemontemayor@live.com>
2019-10-07 18:24:49 -0500

    x

--- src/config.js
+++ src/config.js
@@ -5,6 +5,6 @@ module.exports = {
     JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
     DB_URL: process.env.DB_URL || 'meal_deal@localhost/meal_deal',
-  // DB_URL: process.env.DATABASE_URL || 'meal_deal@localhost/meal_deal',
+  DATABASE_URL: process.env.DATABASE_URL || 'meal_deal@localhost/meal_deal',
 
   }
