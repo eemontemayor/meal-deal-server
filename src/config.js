7753f4099b279965c712ba50cae3a24496576a37
Enrique Montemayor <eemontemayor@live.com>
2019-10-07 16:34:58 -0500

    changed header color

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     JWT_SECRET: process.env.JWT_SECRET,
-  JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
-  DB_URL: process.env.DB_URL || 'meal_deal@localhost/meal_deal',
+    JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
+    DB_URL: process.env.DB_URL || 'meal_deal@localhost/meal_deal',
   // DB_URL: process.env.DATABASE_URL || 'meal_deal@localhost/meal_deal',
 
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
   }
