db614b1841462c7ca4552633e1262e605b0be40e
Enrique Montemayor <eemontemayor@live.com>
2019-10-08 18:49:37 -0500

    switch db url to test out dev

--- src/server.js
+++ src/server.js
@@ -6,15 +6,15 @@ const { PORT, DB_URL, DATABASE_URL } = require('./config');
 
 const db = knex({
   client: 'pg',
-  // connection: DB_URL
-  connection: DATABASE_URL
+  connection: DB_URL
+  // connection: DATABASE_URL
 });
 
 app.set('db', db);
 
 app.listen(PORT, () => {
   
-  // console.log('Db URL: ', DB_URL);
-  console.log('Database URL: ', DATABASE_URL);
+  console.log('Db URL: ', DB_URL);
+  // console.log('Database URL: ', DATABASE_URL);
   console.log(`Server listening at http://localhost:${PORT}`);
 });
