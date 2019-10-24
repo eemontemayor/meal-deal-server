5c1ea7d50c18ea829e462a75c73f4d9329acfe79
Enrique Montemayor <eemontemayor@live.com>
2019-10-24 18:34:24 -0500

    able to post and get isnt and ing on modItems

--- src/meals/meals-services.js
+++ src/meals/meals-services.js
@@ -59,6 +59,7 @@ const mealService = {
             id: meal.id,
             meal_name: meal.meal_name,
             ingredients: meal.ingredients,
+            instructions:meal.instructions,
             image:meal.image,
             on_day: meal.on_day,
             user_id: meal.user_id
