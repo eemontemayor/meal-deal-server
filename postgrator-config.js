7e08fce2089b8162a5de83738542eaba136543ff
eemontemayor <eemontemayor@live.com>
2020-04-01 18:54:35 -0500

    updated npm deps

--- postgrator-config.js
+++ postgrator-config.js
@@ -13,7 +13,7 @@ module.exports = {
   "password": process.env.MIGRATION_DB_PASS,
   "validateChecksums": false,
   "connectionString": (process.env.NODE_ENV === 'test')
-      ? process.env.TEST_DATABASE_URL
-      : process.env.DATABASE_URL,
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
   "ssl": !!process.env.SSL,
 };
