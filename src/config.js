bc002f6a4d293919ec7e4fd308eab2357adce7f6
eemontemayor <eemontemayor@live.com>
2020-04-02 21:20:13 -0500

    moved env file to root folder

--- src/config.js
+++ src/config.js
@@ -1,13 +1,14 @@
 'use strict';
 
+
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
-  // DB_URL: process.env.DB_URL || 'meal_deal@localhost/meal_deal',
-  DB_URL: process.env.DATABASE_URL || 'meal_deal@localhost/meal_deal',
+  DB_URL: process.env.DB_URL ,
+  // DB_URL: process.env.DATABASE_URL || 'meal_deal@localhost/meal_deal',
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || '*',
   APP_ID: process.env.APP_ID,
   API_KEY:process.env.API_KEY,
--- src/meals/meals-router.js
+++ src/meals/meals-router.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express')
 const mealsRouter= express.Router()
 const jsonBodyParser= express.json()
@@ -8,162 +10,162 @@ const cookie = require('cookie');
 const axios = require('axios');
 const config = require('../config');
 mealsRouter
-.get('/', requireAuth, jsonBodyParser,(req,res, next)=>{ 
+  .get('/', requireAuth, jsonBodyParser,(req,res, next)=>{ 
   
     const user_id = req.user.id
   
     mealService.getMeals(
-        req.app.get('db'),
-        user_id
+      req.app.get('db'),
+      user_id
     )
-    .then((meals) => {
+      .then((meals) => {
         return res
-            .status(200)
-            .json(meals.map(i => mealService.serializeMeal(i)))
-    })
-    .catch(next);
-})
+          .status(200)
+          .json(meals.map(i => mealService.serializeMeal(i)))
+      })
+      .catch(next);
+  })
 
 
-.delete('/', jsonBodyParser,(req,res,next)=>{
+  .delete('/', jsonBodyParser,(req,res,next)=>{
     let id = req.body.id
-   console.log(id,'--------')
+    console.log(id,'--------')
     mealService.deleteMeal(
-        req.app.get('db'),
-        id
+      req.app.get('db'),
+      id
     )
-    .then(meal =>{
+      .then(meal =>{
         res
-            .status(204)
-            .end()
-    })
-    .catch(next)
+          .status(204)
+          .end()
+      })
+      .catch(next)
     
   
-})
+  })
 
 mealsRouter
-.get('/explore/:meal', requireAuth, jsonBodyParser,(req,res, next)=>{ 
-  console.log(req.params.meal,'<------------')
+  .get('/explore/:meal', requireAuth, jsonBodyParser,(req,res, next)=>{ 
+    console.log(req.params.meal,'<------------')
     const user_id = req.user.id
 
     axios.get(`https://api.edamam.com/search?q=${req.params.meal}&app_id=${config.APP_ID}&app_key=${config.API_KEY}`, {
-        headers: {
-            'Accept-Encoding': 'gzip',
-            // 'Set-Cookie':  'SameSite=None',
-        }
+      headers: {
+        'Accept-Encoding': 'gzip',
+        // 'Set-Cookie':  'SameSite=None',
+      }
     
-     })
-   
-        .then((meals) => {
-            // console.log(meals.data.hits)
-            // res.cookie('cross-site-cookie', 'bar', { sameSite: 'none', secure: true })
-            return res
-            .cookie('cross-site-cookie', 'bar', { 'SameSite': 'none', secure: true })
-        
-            .status(200)
-            // .json(meals.map(i => mealService.serializeMeal(i)))
-            .json(meals.data.hits)
     })
-    .catch(next);
-})
+   
+      .then((meals) => {
+        // console.log(meals.data.hits)
+        // res.cookie('cross-site-cookie', 'bar', { sameSite: 'none', secure: true })
+        return res
+          .cookie('cross-site-cookie', 'bar', { 'SameSite': 'none', secure: true })
+        
+          .status(200)
+        // .json(meals.map(i => mealService.serializeMeal(i)))
+          .json(meals.data.hits)
+      })
+      .catch(next);
+  })
 //=====================================================
 
 mealsRouter
-.get('/:date', requireAuth, jsonBodyParser,(req,res, next)=>{ 
+  .get('/:date', requireAuth, jsonBodyParser,(req,res, next)=>{ 
   
     const on_day=req.params.date
     const user_id = req.user.id
 
     mealService.getMealsByDate(
-        req.app.get('db'),
-        user_id,
-        on_day
+      req.app.get('db'),
+      user_id,
+      on_day
     )
-    .then((meals) => {
+      .then((meals) => {
         
         return res
-            .status(200)
-            .json(meals.map(i => mealService.serializeMeal(i)))
-    })
-    .catch(next);
-})
+          .status(200)
+          .json(meals.map(i => mealService.serializeMeal(i)))
+      })
+      .catch(next);
+  })
 
 mealsRouter
-.get('/:date/:meal_id', requireAuth, jsonBodyParser , (req,res,next)=>{
+  .get('/:date/:meal_id', requireAuth, jsonBodyParser , (req,res,next)=>{
     
     const id=req.params.meal_id
     const user_id = req.user.id
     mealService.getMealById(
-        req.app.get('db'),
-        user_id,
-        id
+      req.app.get('db'),
+      user_id,
+      id
     )
-    .then((meals) => {
+      .then((meals) => {
         
         return res
-            .status(200)
-            .json(meals.map(i => mealService.serializeMeal(i)))
-    })
-    .catch(next);
-})
+          .status(200)
+          .json(meals.map(i => mealService.serializeMeal(i)))
+      })
+      .catch(next);
+  })
 
-.patch('/:meal_id', requireAuth, jsonBodyParser, (req,res,next)=>{
+  .patch('/:meal_id', requireAuth, jsonBodyParser, (req,res,next)=>{
     const user_id = req.user.id
     const {meal}= req.body
     const id= req.body.id
     const updatedMeal = { meal }
 
-       const numberOfValues = Object.values(updatedMeal).filter(Boolean).length
-       if (numberOfValues === 0) {
-         return res.status(400).json({
-           error: {
-             message: `Request body must contain either 'title', 'style' or 'content'`
-           }
-         })
-       }
+    const numberOfValues = Object.values(updatedMeal).filter(Boolean).length
+    if (numberOfValues === 0) {
+      return res.status(400).json({
+        error: {
+          message: `Request body must contain either 'title', 'style' or 'content'`
+        }
+      })
+    }
 
     mealService.updateMeal(
-        req.app.get('db'),
-        id,
-        updatedMeal
+      req.app.get('db'),
+      id,
+      updatedMeal
     )
     then(meal => {
-        res.status(200).json(serializeTodo(meal[0]))
+      res.status(200).json(serializeTodo(meal[0]))
+    })
+
+      .then(numRowsAffected => {
+        res.status(204).end()
       })
+      .catch(next)
 
-    .then(numRowsAffected => {
-          res.status(204).end()
-           })
-            .catch(next)
-
-})
+  })
 
 
 mealsRouter
-.post('/:date', requireAuth, jsonBodyParser, (req,res,next)=>{
+  .post('/:date', requireAuth, jsonBodyParser, (req,res,next)=>{
    
     const newMeal = req.body;
-     newMeal.on_day=req.params.date
+    newMeal.on_day=req.params.date
     newMeal.user_id = req.user.id;
     const user_id = req.user.id
 
     console.log(newMeal)
     mealService.insertMeal(
-        req.app.get('db'),
-        newMeal,
-        user_id
+      req.app.get('db'),
+      newMeal,
+      user_id
      
     )
-    .then((meal) => { 
+      .then((meal) => { 
         res
-            .status(201)
-            .location(path.posix.join(req.originalUrl, `/${meal.id}`))
-            .json(mealService.serializeMeal(meal))
-    }) 
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${meal.id}`))
+          .json(mealService.serializeMeal(meal))
+      }) 
   
-    .catch(next);
-})
+      .catch(next);
+  })
 
 
 module.exports = mealsRouter
